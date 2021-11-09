f1f1fbae8de4c556a4fdc9cf9775f9103715edd2
Nick Seddon <nickseddon187@gmail.com>
2021-11-08 20:12:17 -0500

    Module 11.6.2 push

--- static/js/app.js
+++ static/js/app.js
@@ -4,24 +4,25 @@ const tableData = data;
 // Reference the HTML table using d3
 var tbody = d3.select("tbody");
 
-function buildTable(data){
-    // First, clear out any existing data.
+function buildTable(data) {
+    // First, clear out any existing data
     tbody.html("");
-
-    //Next, loop through each object in the data
-    //and append a row and cells for each value in the row.
+  
+    // Next, loop through each object in the data
+    // and append a row and cells for each value in the row
     data.forEach((dataRow) => {
-        //Append a row to the table body.
-        let row = tbody.append("tr");
-        
-        //Loop through each field in the dataRow and add
-        //each value as a table cell (td)
-        Object.values(dataRow).forEach((val) => {
-            let cell = row.append("td");
-            cell.text(val);
-        });
+      // Append a row to the table body
+      let row = tbody.append("tr");
+  
+      // Loop through each field in the dataRow and add
+      // each value as a table cell (td)
+      Object.values(dataRow).forEach((val) => {
+        let cell = row.append("td");
+        cell.text(val);
+        }
+      );
     });
-}
+  }
 
 function handleClick() {
     // Grab the datetime value from the filter
@@ -34,17 +35,16 @@ function handleClick() {
       // Apply `filter` to the table data to only keep the
       // rows where the `datetime` value matches the filter value
       filteredData = filteredData.filter(row => row.datetime === date);
-    };
+    }
   
      // Rebuild the table using the filtered data
     // @NOTE: If no date was entered, then filteredData will
     // just be the original tableData.
     buildTable(filteredData);
 }
-
+  
 // Attach an event to listen for the form button
 d3.selectAll("#filter-btn").on("click", handleClick);
-
+  
 // Build the table when the page loads
 buildTable(tableData);
-}
